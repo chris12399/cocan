c5e5ee2fa62e0218a00e4ed1db4a22269109d54d
User18 <user18@user18de-imac.local>
2018-10-31 14:30:15 +0800

    Initial Commit

---
+++ 1031 star/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1031 star
+//
+//  Created by User18 on 2018/10/31.
+//  Copyright © 2018 User18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
